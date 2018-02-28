acafd0103fc29cb998b1063a6ff5a0c40b46548a
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 15:55:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.length == 2
-    array.join("and")
+    array.join(" and ")
   elsif array.length == 1
     array.to_s
 end
 end
+
+# a.map { |i|  + i.to_s + }.join(",")
